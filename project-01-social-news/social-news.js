927f42ffe9d5443102b8fd8e6cc0d83c96d6b232
adnanekouna-a <adnanekouna.a@gmail.com>
2022-06-29 12:09:39 +0100

    Implement auto-add http to url

--- project-01-social-news/social-news.js
+++ project-01-social-news/social-news.js
@@ -14,9 +14,17 @@ const between = (startNum, endNum) => {
 		return `(between ${Math.min(startNum, endNum)} and ${Math.max(startNum, endNum)})`
 	}
 };
+// Range function to check if the removedNum is in it
+const range = (startNum, endNum) => [...Array(endNum).keys()].slice(startNum)
 // To check whether the number is valid in a range or not (Broken)
-const isNumberUnvalid = (number, min, max) =>  ((number < min) || number > max || (typeof number != "number" && number != ""));
+const isNumberUnvalid = (number, min, max) =>  !(number > min && number < max && (typeof number == "number" || number == ""));
 
+const formatUrl = url => {
+	if(!(url.startsWith("http://") || url.startsWith("https://"))){
+		return "http://" + url
+	}
+	return url
+}
 
 // The messgaes
 const promptMsg = `Choose an option:
@@ -29,6 +37,7 @@ const addUrlMsg = "Enter the URL:";
 const addAuthorMsg = "Enter the link's author:";
 const removeLinkMsg = "Enter the index of the link to be removed: ";
 const noLinksMsg = "There are no added links :\'(";
+
 
 // Main Program
 while(1) {
@@ -49,16 +58,17 @@ while(1) {
 		// Adds a newLink object to store it in the links array
 		const newLink = {};
 		newLink.title = prompt(addTitleMsg);
-		newLink.url = prompt(addUrlMsg);
+		newLink.url = formatUrl(prompt(addUrlMsg));
 		newLink.author = prompt(addAuthorMsg);
 		links.push(newLink);
 		continue;
 	} else if(Number(actionChoice == 3)) {
 		// Removes the link by index if provided correctly, otherwise it prompts again (Broken)
 		let removedLink = 0;
-		do {
-			removedLink = prompt(removeLinkMsg+between(1,links.length||1));
-		} while(isNumberUnvalid(removedLink, 0, links.length+1));
+		while(!(removedLink in range(1,links.length+1))){
+			removedLink = Number(prompt("Give me the damn number"));
+			console.log(removedLink);
+		};
 		links.pop(removedLink-1);
 		continue;
 	} else {
@@ -68,4 +78,3 @@ while(1) {
 
 // TODO
 // - Fix the link removal mechanism
-// - Add the functionality to add https:// in links if not there
